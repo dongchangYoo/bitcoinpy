c8d4637fe1e54982f7234de058d52aa1a07df298
dongchangYoo <dongchang.yoo@pi-lab.io>
2021-08-17 15:12:56 +0900

    cleanup bytes class

--- bitcoinpy/base/bytes.py
+++ bitcoinpy/base/bytes.py
@@ -2,7 +2,7 @@ class BTCBytes:
     def __init__(self, big_bytes: bytes):
         if not isinstance(big_bytes, bytes):
             raise Exception("Wrong input format. expected: {}, acture: {}".format("bytes", type(big_bytes)))
-        self._big_bytes = big_bytes  # store as little endian
+        self._big_bytes = big_bytes  # store as big endian
 
     def __repr__(self):
         return "be({})".format(self. _big_bytes.hex())
@@ -22,17 +22,21 @@ class BTCBytes:
             raise Exception("other type is not BTCBytes")
         return not self == other
 
-    @staticmethod
-    def type_check(other):
-        return True if isinstance(other, BTCBytes) else False
+    @classmethod
+    def from_little_bytes(cls, little_bytes: bytes):
+        return cls(little_bytes[::-1])
 
     @classmethod
     def from_big_hex(cls, big_hex: str):
+        if big_hex.startswith("0x"):
+            big_hex = big_hex[2:]
         data = bytes.fromhex(big_hex)
         return cls(data)
 
     @classmethod
     def from_little_hex(cls, little_hex: str):
+        if little_hex.startswith("0x"):
+            little_hex = little_hex[2:]
         data = bytes.fromhex(little_hex)[::-1]
         return cls(data)
 
@@ -58,4 +62,8 @@ class BTCBytes:
 
     @property
     def int(self):
-        return int.from_bytes(self.bytes_as_be, 'little')
+        return int.from_bytes(self.bytes_as_be, 'big')
+
+    @staticmethod
+    def type_check(other):
+        return True if isinstance(other, BTCBytes) else False
